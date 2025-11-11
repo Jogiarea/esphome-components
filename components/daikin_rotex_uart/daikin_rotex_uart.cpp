f8bb94eda68e703daa548071f668cb14927deb98
Jogiarea <86875561+Jogiarea@users.noreply.github.com>
2025-11-11 10:12:14 +0100

    Refactor add_entity method for cleaner logic

--- components/daikin_rotex_uart/daikin_rotex_uart.cpp
+++ components/daikin_rotex_uart/daikin_rotex_uart.cpp
@@ -23,14 +23,8 @@ DaikinRotexUARTComponent::DaikinRotexUARTComponent()
 
 
 void DaikinRotexUARTComponent::add_entity(EntityBase* pEntityBase) {
-    if (auto pSensor = dynamic_cast<daikin_rotex_uart::UartSensor*>(pEntityBase)) {
-        m_message_manager.add(pSensor);
-    } else if (auto pTextSensor = dynamic_cast<daikin_rotex_uart::UartTextSensor*>(pEntityBase)) {
-        m_message_manager.add(pTextSensor);
-    } else if (auto pBinarySensor = dynamic_cast<daikin_rotex_uart::UartBinarySensor*>(pEntityBase)) {
-        m_message_manager.add(pBinarySensor);
-    } else if (auto pEntity = dynamic_cast<daikin_rotex_uart::TEntity*>(pEntityBase)) {
-        m_message_manager.add(pEntity); // Fallback
+    if (auto pEntity = dynamic_cast<daikin_rotex_uart::TEntity*>(pEntityBase)) {
+        m_message_manager.add(pEntity);
     }
 }
 
